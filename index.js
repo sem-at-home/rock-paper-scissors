a0053aec18d7bfc2d5a4f0ceab48e33d2beeb723
Sem Erik Joop van Velden <vanveldensem@gmail.com>
2024-07-01 22:13:41 +0200

    update index.js to return computer choice in console.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('Hello World')
+
+console.log(getComputerChoice())
+
+function getComputerChoice() {
+    const possibleChoices = ['rock', 'paper', 'scissors']
+    const randomChoice = Math.floor(Math.random() * possibleChoices.length)
+    return possibleChoices[randomChoice]
+}
