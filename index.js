70e11a92e20b079b68f190298e7c34f6f3224b57
Sem Erik Joop van Velden <vanveldensem@gmail.com>
2024-07-01 22:16:21 +0200

    update index.js with a prompt to get the player's choice

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ console.log('Hello World')
 
 console.log(getComputerChoice())
 
+getHumanChoice()
+
 function getComputerChoice() {
     const possibleChoices = ['rock', 'paper', 'scissors']
     const randomChoice = Math.floor(Math.random() * possibleChoices.length)
     return possibleChoices[randomChoice]
+}
+
+function getHumanChoice() {
+    console.log(prompt('Rock, Paper or Scissors?'))
 }
